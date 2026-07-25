dcaa0724d1519979222dde0269ee0a95b98bc07a
Pavan B <pavaneb20052gmail.com>
2026-07-25 12:58:37 +0530

    Created products listing page

--- app/app.py
+++ app/app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from db import get_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/products")
+def products():
+
+    connection = get_connection()
+
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Product")
+
+    products = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return render_template("products.html", products=products)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/db.py
+++ app/db.py
@@ -1,19 +1,9 @@
 import mysql.connector
 
-try:
-    connection = mysql.connector.connect(
+def get_connection():
+    return mysql.connector.connect(
         host="localhost",
         user="root",
         password="Mysql@123",
         database="RetailManagement"
     )
-
-    if connection.is_connected():
-        print("✅ Connected to MySQL successfully!")
-
-except Exception as e:
-    print("❌ Error:", e)
-
-finally:
-    if 'connection' in locals() and connection.is_connected():
-        connection.close()
