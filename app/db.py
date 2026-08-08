8a3c48a5a738a7d00c8fa830492042af6c9f058e
Pavan B <pavaneb20052gmail.com>
2026-08-08 20:01:37 +0530

    Secure database configuration

--- app/db.py
+++ app/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Mysql@123",
+        password="YOUR_PASSWORD",
         database="RetailManagement"
     )
